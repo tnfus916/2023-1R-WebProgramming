b30fd93f902bd559de4ba7fcae8677899a260f7c
tnfus916 <tnfus1999@gmail.com>
2023-05-21 19:23:58 +0900

    main.jsx 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Worldcup from './Worldcup'
+import Worldcup from './WorldcupMay21'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
